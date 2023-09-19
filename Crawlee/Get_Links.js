b466019c730509d37d3e861ad03fe16a99e1a764
bhumik1310 <92202172+bhumik1310@users.noreply.github.com>
2023-09-19 14:58:59 +0530

    Added the Parts of crawler built

---
+++ Crawlee/Get_Links.js
@@ -0,0 +1,18 @@
+import { Dataset,CheerioCrawler } from 'crawlee';
+
+const crawler = new CheerioCrawler({
+    async requestHandler({ request, enqueueLinks, log , body}) {
+        log.info(request.url);
+        await Dataset.pushData({
+            url: request.url,
+            
+        });
+        // Add all links from page to RequestQueue
+        await enqueueLinks();
+    },
+    maxRequestsPerCrawl: 50, // Limitation for only 10 requests (do not use if you want to crawl all links)
+});
+
+// Run the crawler with initial request
+await crawler.run(['https://www.kamdhenuuni.edu.in/']);
+await Dataset.exportToCSV('OUTPUT', { toKVS: 'my-data' }); //Outputs the links in the subdomain to the respective CSV file
